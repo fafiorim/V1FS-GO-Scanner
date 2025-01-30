0fca09ffb2d0d97c1a3d9b2ad2effaa38583053a
fafiorim <franzvitor@gmail.com>
2025-01-29 21:24:37 -0800

    enhance build binaries

--- tools/build_binaries.sh
+++ tools/build_binaries.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
- 
+
 # Set the name of your main Go file
 MAIN_FILE="../main.go"
 OUTPUT_DIR="bin"
- 
+
 # Ensure the output directory exists
-mkdir -p $OUTPUT_DIR
- 
-# Build for Linux amd64
-echo "Building for Linux amd64..."
-GOOS=linux GOARCH=amd64 go build -o $OUTPUT_DIR/v1_fs_scanner_linux $MAIN_FILE
- 
-# Build for Linux arm
-echo "Building for Linux ARM..."
-GOOS=linux GOARCH=arm64 go build -o $OUTPUT_DIR/v1_fs_scanner_linux_arm $MAIN_FILE
- 
-# Build for Windows
-echo "Building for Windows..."
-GOOS=windows GOARCH=amd64 go build -o $OUTPUT_DIR/v1_fs_scanner_windows.exe $MAIN_FILE
- 
-# Build for macOS amd64
-echo "Building for macOS amd64..."
-GOOS=darwin GOARCH=amd64 go build -o $OUTPUT_DIR/v1_fs_scanner_macos $MAIN_FILE
- 
-# Build for macOS ARM
-echo "Building for macOS ARM..."
-GOOS=darwin GOARCH=arm64 go build -o $OUTPUT_DIR/v1_fs_scanner_macos_arm $MAIN_FILE
- 
+mkdir -p "$OUTPUT_DIR"
+
+# Define build targets
+declare -a TARGETS=(
+    "linux amd64 v1_fs_scanner_linux"
+    "linux arm64 v1_fs_scanner_linux_arm"
+    "windows amd64 v1_fs_scanner_windows.exe"
+    "darwin amd64 v1_fs_scanner_macos"
+    "darwin arm64 v1_fs_scanner_macos_arm"
+)
+
+# Build function
+build() {
+    local os="$1"
+    local arch="$2"
+    local output="$OUTPUT_DIR/$3"
+
+    echo "Building for $os $arch..."
+    GOOS="$os" GOARCH="$arch" go build -o "$output" "$MAIN_FILE"
+    
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Build failed for $os $arch."
+        exit 1
+    fi
+}
+
+# Run builds
+for target in "${TARGETS[@]}"; do
+    build $target
+done
+
+# Print success message only if all builds succeed
 echo "Builds completed successfully."
- 
